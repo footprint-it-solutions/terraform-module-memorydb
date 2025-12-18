29f3644a4f109ab352dd43e1cc27eb90528992f4
Alex Sunins <alex@footprintit.net>
2025-12-18 10:26:26 +0000

    rm cluster_endpoint_address output

--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,3 @@ output "id" {
   description = "The ID of the MemoryDB cluster"
   value       = aws_memorydb_cluster.this.id
 }
-
-output "cluster_endpoint_address" {
-  description = "The DNS hostname of the cluster configuration endpoint"
-  value       = aws_memorydb_cluster.this.cluster_endpoint.address
-}
